7df826e7198fd0dcd59d6985d9dec40c05db45a9
Sanju Kamat <SanjuKamat@gmail.com>
2023-10-12 12:44:44 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,231 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-reception",
+      "name": "Reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.71666872197261,
+        "pitch": 0.08143447987484365,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.54440131746067,
+          "pitch": 0.126622349361881,
+          "rotation": 0,
+          "target": "1-reception"
+        },
+        {
+          "yaw": 1.4476678432052097,
+          "pitch": 0.07188073823044938,
+          "rotation": 0,
+          "target": "2-reception"
+        },
+        {
+          "yaw": -0.2150687485752254,
+          "pitch": 0.11640523215620746,
+          "rotation": 0,
+          "target": "3-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-reception",
+      "name": "Reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.6135323993913779,
+        "pitch": 0.009132093642939765,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3599707585030565,
+          "pitch": 0.0923575545360329,
+          "rotation": 0,
+          "target": "3-reception"
+        },
+        {
+          "yaw": 1.3732786612151546,
+          "pitch": 0.07393684671021816,
+          "rotation": 0,
+          "target": "2-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-reception",
+      "name": "Reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.0601968971704494,
+        "pitch": 0.025766061209660407,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3496687162838903,
+          "pitch": 0.05292134791321246,
+          "rotation": 5.497787143782138,
+          "target": "4-reception"
+        },
+        {
+          "yaw": -2.1713161535702667,
+          "pitch": 0.07494456598344001,
+          "rotation": 0,
+          "target": "0-reception"
+        },
+        {
+          "yaw": -2.999438580277687,
+          "pitch": 0.09612460596416028,
+          "rotation": 0,
+          "target": "1-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-reception",
+      "name": "Reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.15138059838726292,
+        "pitch": 0.05698473213274724,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.653545249263251,
+          "pitch": 0.06286321126211547,
+          "rotation": 0,
+          "target": "0-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-reception",
+      "name": "Reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.6648229690583563,
+        "pitch": 0.00028666555394352145,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5296204192134759,
+          "pitch": 0.09611927084786842,
+          "rotation": 0,
+          "target": "0-reception"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Reception",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
